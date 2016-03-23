2884b6f3f10cde4665d1340d673c81bd5fbd7a86
Marvki <marvkipi@gmail.com>
2016-03-22 22:11:12 -0700

    slide box

--- www/js/app.js
+++ www/js/app.js
@@ -23,9 +23,14 @@ angular.module('App', ['ionic'])
       url: '/restaurants',
       controller: 'RestaurantsController',
       templateUrl: 'views/restaurants/restaurants.html'
+    })
+
+    .state('tour', {
+      url: '/tour',
+      templateUrl: 'views/tour/tour.html'
     });
 
-	 $urlRouterProvider.otherwise('/home');
+	 $urlRouterProvider.otherwise('/tour');
 })
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
